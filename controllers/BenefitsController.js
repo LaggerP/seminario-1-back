8f24f3cb55f7f061a38fe5f4dbdbbb62a31ad44f
LaggerP <pablo_lagger@hotmail.com>
2020-10-24 00:27:03 -0300

    added sender email benefit

--- controllers/BenefitsController.js
+++ controllers/BenefitsController.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const benefits = require('../models').Benefits
+const user = require('../models').User
 const profileControlller = require('../controllers/UserProfileController');
+const mailerController = require('../controllers/mailer/MailerControlles');
+
 module.exports = {
    list(_, res) {
       return benefits.findAll({})
@@ -8,7 +11,7 @@ module.exports = {
          .catch(error => res.status(400).send(error))
    },
    async redeemBenefit(req, res) {
-      const { profile_points, id, id_profile } = req.body
+      const { profile_points, id, id_profile, id_user } = req.body
       const _benefit = await benefits.findOne({ where: { id: id } });
 
       if ((profile_points - _benefit.dataValues.necessary_points) < 0) res.status(400).send({ status: 400, msg: "insufficient points"})
@@ -20,9 +23,9 @@ module.exports = {
             profile_points: profile_points,
             necessary_points: _benefit.dataValues.necessary_points
          }
-         
+         const _user = await user.findOne({where: {id: id_user}})
          const _profile = await profileControlller.updateProfilePoints(dataToUpdate);
-
+         await mailerController.sendBenefitEmail(_user.dataValues)
          res.status(201).send({ msg: 'benefit redeem success' })
 
       } catch (e) {
